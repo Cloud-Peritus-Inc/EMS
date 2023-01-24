932391f0d52f31b80f6bb9120fcfa771627f5fde
srirangakulkarni-cp <sriranga.kulkarni@cloudperitus.com>
2023-01-24 18:06:06 +0530

    QA Deployment Ready
    QA Deployment Ready

---
+++ force-app/main/default/lwc/eMS_LeaveRequest_Table/eMS_LeaveRequest_Table.js
@@ -0,0 +1,124 @@
+import { LightningElement, api } from 'lwc';
+import updateRejecteStatusAndComments from '@salesforce/apex/LeaveRequestRejectHandler.updateRejecteStatusAndComments';
+import updateApproveStatusAndComments from '@salesforce/apex/LeaveRequestApproveHandler.updateApproveStatusAndComments';
+//import bulkLeaveReqApproval from '@salesforce/apex/LeaveRequestApproveHandler.bulkLeaveReqApproval';
+export default class EMS_LeaveRequest_Table extends LightningElement {
+
+    approveComments;
+    rejectComments;
+    multipleApprovals = []
+    isShowModalApprove = false;
+    isShowModalReject = false;
+    isViewAll = false;
+    _leaveReqData;
+    @api get leaveReq() {
+        return this._leaveReqData;
+    }
+
+    set leaveReq(value) {
+        this._leaveReqData = this.leaveData(value);
+    }
+
+    leaveData(value) {
+        value = JSON.parse(JSON.stringify(value));
+        value.forEach((item, index) => {
+            console.log('### item size: ' + JSON.stringify(item))
+        });
+        return value;
+    }
+    connectedCallback() {
+        this.leaveReq = JSON.parse(JSON.stringify(this.leaveReq));
+        this.leaveReq.forEach((item, index) => {
+            console.log('### item size: ' + JSON.stringify(item))
+        });
+
+        //Conditions to make isViewAll to TRUE
+        var url = new URL("https://cpprd--dev.sandbox.my.site.com/CpLink/s/all-approvals");
+        var params = new URLSearchParams();
+        params.append("bulkupdate", "value");
+        url.search += "&" + params.toString();
+        var params = new URLSearchParams(location.search);
+        if (params.has('bulkupdate')) {
+            console.log("Hello");
+            this.isViewAll = true;
+            console.log('Bye : ');
+        }
+    }
+
+    //CheckBox
+    handleSelect(event) {
+        console.log('SELECTED RECORD : ');
+        const selectedRecordCheckboxId = event.currentTarget.dataset.id;
+        //  this.multipleApprovals.push(selectedRecordCheckboxId);
+        this.multipleApprovals = [... this.multipleApprovals, selectedRecordCheckboxId];
+        console.log('###  multipleApprovals Child: ', this.multipleApprovals);
+        const myApproveAllEvent = new CustomEvent('approveall', {
+            detail: this.multipleApprovals
+        });
+        this.dispatchEvent(myApproveAllEvent);
+    }
+
+    //Approve Modal
+    handleApproveComments(event) {
+        this.approveComments = event.target.value;
+    }
+
+    showModalApprovalBox() {
+        console.log('BUTTON CLICKED : ');
+        this.isShowModalApprove = true;
+    }
+
+    handleApproveSave(event) {
+        const selectedRecordApproveId = event.currentTarget.dataset.id;
+        console.log('### selectedRecordApproveId : ', selectedRecordApproveId);
+        updateApproveStatusAndComments({ leaveRequestId: selectedRecordApproveId, comments: this.approveComments })
+            .then((result) => {
+                console.log('Leave Request: ', result);
+                window.location.reload();
+            }).catch((err) => {
+                console.log('ERROR : ', err);
+            });
+    }
+
+    //Reject Modal
+    handleRejectComments(event) {
+        this.rejectComments = event.target.value;
+    }
+
+    showModalRejectBox() {
+        console.log('BUTTON CLICKED : ');
+        this.isShowModalReject = true;
+    }
+
+    handleRejectSave(event) {
+        let getelement = this.template.querySelectorAll(".isRequired");
+        console.log("getelement", JSON.stringify(getelement));
+        let letReject = true;
+        getelement.forEach(currentItem => {
+            if (!currentItem.value || !currentItem.checkValidity()) {
+                currentItem.reportValidity();
+                console.log(" currentItem.reportValidity();", currentItem.reportValidity());
+                currentItem.setCustomValidity("Please entre this fields");
+                letReject = false;
+            }
+        });
+
+        if (letReject) {
+            const selectedRecordRejectId = event.currentTarget.dataset.id;
+            console.log('### selectedRecordRejectId : ', selectedRecordRejectId);
+            updateRejecteStatusAndComments({ leaveRequestId: selectedRecordRejectId, comments: this.rejectComments })
+                .then((result) => {
+                    console.log('Leave Request: ', result);
+                    this.isShowModalReject = false;
+                    window.location.reload();
+                }).catch((err) => {
+                    console.log('ERROR : ', err);
+                });
+        }
+    }
+
+    handleClose() {
+        this.isShowModalApprove = false;
+        this.isShowModalReject = false;
+    }
+}
---
+++ force-app/main/default/triggers/CP_LeaveCreditTrigger.trigger
@@ -0,0 +1,22 @@
+/**
+* @author Eswar
+* @date 11/01/2023
+* @description Master Trigger for the Leave Credit object, which fires on ALL Trigger
+*              actions to control the order in which they occur.
+*
+* CHANGE HISTORY
+* ====================================================================================================
+* DATE          NAME                    DESCRIPTION
+* N/A           N/A                     N/A
+* ====================================================================================================
+**/
+trigger CP_LeaveCreditTrigger on EMS_LM_Leave_Credit__c (before insert,
+  after insert,
+  before update,
+  after update,
+  before delete,
+  after delete,
+  after undelete
+) {
+  new MetadataTriggerHandler().run();
+}
