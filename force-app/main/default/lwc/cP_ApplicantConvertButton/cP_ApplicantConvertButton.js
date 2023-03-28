36bd107b979c9e40e6e54f52dd17a66ba2652ef1
krishnaa12b <nagavamsikrishna88@gmail.com>
2023-03-28 13:54:12 +0530

    28th-1st - diployment

--- force-app/main/default/lwc/cP_ApplicantConvertButton/cP_ApplicantConvertButton.js
+++ force-app/main/default/lwc/cP_ApplicantConvertButton/cP_ApplicantConvertButton.js
@@ -1,8 +1,6 @@
 import { LightningElement,track,wire,api } from 'lwc';
 import recordTypeChangeByLWC from '@salesforce/apex/CP_aplicantConvertResource.recordTypeChangeByLWC';
-import convertButton from '@salesforce/apex/CP_aplicantConvertResource.ConvertButton';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import { getRecord, updateRecord, getFieldValue } from "lightning/uiRecordApi";
 import { NavigationMixin } from 'lightning/navigation';
 
 export default class CP_ApplicantConvertButton extends NavigationMixin(LightningElement) {
@@ -10,55 +8,49 @@ export default class CP_ApplicantConvertButton extends NavigationMixin(Lightning
    conid;
 
    
-    updateRecordType(){
+     updateRecordType(){
         console.log('recordId-->',this.recordId);
        
-         const value = true;
+        const value = true;
         const valueChangeEvent = new CustomEvent("valuechange", {
-          detail: { value }
-     });
+            detail: { value }
+        });
         // Fire the custom event
-         this.dispatchEvent(valueChangeEvent);
+        this.dispatchEvent(valueChangeEvent);
+        
         recordTypeChangeByLWC({contactId:this.recordId})
         .then(res => {
             console.log('this.res-->',res);
-            this.conid =res[0].Id;
-            console.log('this.conid1-->',this.conid);
-            //console.log('this.conid3-->',res);
-
-        convertButton({contactId:this.recordId})
-        .then(result => {
-            this.recordId = result;  
+            this.conid = res[0].Id;
+            
             this.dispatchEvent(
-              new ShowToastEvent({
-                  title: 'Success',
-                  message: 'Applicant converted to Resource successfully!',
-                  variant: 'success',
-              }),
-          );window.location.reload();
-        //   this.dispatchEvent( new CustomEvent('closeQuickAction'));
-        //   this[NavigationMixin.Navigate]({
-        //     type: 'standard__webPage',
-        //     attributes: {
-        //         url: `/detail/${this.recordId}`
-        //     }
-        // },
-        // true
-        // );
-          
-        })
-        .catch(error => {
-            this.error = error;
-            console.log('this.errors-->'+JSON.stringify(this.error));
-        });
-           
-          
+                new ShowToastEvent({
+                    title: 'Success',
+                    message: 'Applicant converted to Resource successfully!',
+                    variant: 'success',
+                })
+            );
+            this.updateRecordView();
+            this[NavigationMixin.Navigate]({
+                type: 'standard__recordPage',
+                attributes: {
+                    recordId: this.recordId,
+                    objectApiName: 'Contact',
+                    actionName: 'view'
+                }
+            }, true /*refresh*/);
         })
         .catch(error => {
             this.error = error;
             console.log('this.errorsss-->'+JSON.stringify(this.error));
         });
-}
+    }
+
+    updateRecordView() {
+       setTimeout(() => {
+            eval("$A.get('e.force:refreshView').fire();");
+       }, 1000); 
+    }
 
 handleCancel(){
     this.dispatchEvent( new CustomEvent('closeQuickAction'));
