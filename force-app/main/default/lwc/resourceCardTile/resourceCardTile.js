28d5b5284e0496fa7222ab630825f16080d489ed
krishnaa12b <121793651+srirangakulkarni-cp@users.noreply.github.com>
2023-04-20 22:33:53 +0530

    QA commits

---
+++ force-app/main/default/lwc/resourceCardTile/resourceCardTile.js
@@ -0,0 +1,52 @@
+import { LightningElement,api,wire } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+import getUserInfo from '@salesforce/apex/resourceTileController.getUserInfo';
+import paydateimage from '@salesforce/resourceUrl/NextPaydate';
+export default class ResourceCardTile extends NavigationMixin(LightningElement)  {
+paydateimage = paydateimage;
+imagelogo = '';
+userdate;
+userImageUrl;
+resourceName = '';
+resourceId = '';
+resourcereportto = '';
+nextpaydate;
+resourcerole = '';
+userid= '';
+resourceinsurence = '';
+rescodecurrency = '';
+resourceContactId = '';
+
+
+
+    @wire(getUserInfo)
+    userInfo({ error, data }) {
+    if (data) {
+      this.userdate = data;
+      this.userImageUrl = data.resourcePhotoURL;
+      this.resourceName = data.resourceName;
+      this.resourceId = data.resourceCpId;
+      this.resourcereportto = data.resourceReportTo;
+      this.nextpaydate = data.nextpaydate;
+      this.resourcerole = data.resourceRole;
+      this.userid = data.resourceUserId;
+      this.resourceinsurence = data.resourceInsurence;
+      this.rescodecurrency = data.rescodecurrency;
+      this.resourceContactId = data.resourceContactId;
+    } else if (error) {
+      console.error(error);
+    }
+  }
+
+
+   handleClick() {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__webPage',
+            attributes: {
+                url: '/detail/'+this.resourceContactId
+            }
+        });
+}
+
+
+}
