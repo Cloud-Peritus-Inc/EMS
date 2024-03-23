761c3b519790041f4e409172fd2446c9e002efdf
krishnaa12b <121793651+srirangakulkarni-cp@users.noreply.github.com>
2024-03-23 23:48:01 +0530

    23thMarch

---
+++ force-app/main/default/lwc/myawards/myawards.js
@@ -0,0 +1,5 @@
+import { LightningElement,api } from 'lwc';
+export default class Myawards extends LightningElement {
+@api awardtable = [];
+@api showtable;
+}
---
+++ force-app/main/default/lwc/performancemgntmain/performancemgntmain.js
@@ -0,0 +1,20 @@
+import { LightningElement,wire } from 'lwc';
+import checkTheTeam from '@salesforce/apex/myTeamController.checkTheTeam';
+export default class Performancemgntmain extends LightningElement {
+ dontshowTheTeam = false;
+connectedCallback() {
+    this.callandCheckTheTeam();
+    console.log('In Performance one');
+}
+//  //Commented for my team tab hiding when the HR profile having custom permission 'Performance Management Admin'--Praveen
+callandCheckTheTeam() {  
+          checkTheTeam()
+         .then(result => {
+               console.log('-====data Perf--=-=-'+JSON.stringify(result));
+        this.dontshowTheTeam = result;  
+         })
+         .catch(error => {
+            console.log('====Error======='+JSON.stringify(error));
+         });    
+}
+}
